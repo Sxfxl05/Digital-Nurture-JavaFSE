25edab18793d368632e641761f4c3cab7fdb2c48
Sxfxl05 <safalchaturvedi822@gmail.comv>
2026-06-30 21:44:16 +0530

    Completed Exercise 5: Migrated manual bean configurations to stereotype annotations

--- LibraryManagement/src/main/java/com/library/repository/BookRepository.java
+++ LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -1,5 +1,9 @@
 package com.library.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
+
 public class BookRepository {
     public void saveData() {
         System.out.println("[BookRepository] Successfully connecting to database and saving book records!");
